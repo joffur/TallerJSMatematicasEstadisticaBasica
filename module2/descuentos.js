4bd84b60eb1d0c65e177ebc404457fb0c7d4eaf8
Jose Fernando Fuentes Roa <ing.josefuentes@hotmail.com>
2023-02-11 11:47:10 -0500

    Pagina de calculadora de porcentajes. Seccion de calculadora de nuevo precio con cupon. Falta terminar de estilizar algunas secciones y cacular el precio con base al cupon ingresado

--- module2/descuentos.js
+++ module2/descuentos.js
@@ -1,9 +1,21 @@
-const btn = document.querySelector('#calculate');
+// Charge of page
+const cuponsAvailable = document.querySelector('.coupon-discount__available');
+
+// Discount variables
 const inputPrice = document.querySelector('#price');
 const inputDiscount = document.querySelector('#discount');
 const resultContainer = document.querySelector('#result-container');
 const pResult = document.querySelector('#result');
 
+
+// Coupon variables
+const inputPrice2 = document.querySelector('#price-2');
+const inputCoupon = document.querySelector('#coupon');
+const couponResultContainer = document.querySelector('#coupon-result-container');
+const btn = document.querySelector('#calculateCoupon');
+const pCouponresult = document.querySelector('#coupon-result');
+
+// Dinamic display of coupons
 const coupons = {
   'WinterSale': 30,
   'Spooky': 20,
@@ -12,6 +24,22 @@ const coupons = {
   'Enjoy': 10,
 };
 
+function displayCouponsAvailable(obj) {
+  const unsignedList = document.createElement('ul');
+  for (const coupon in obj) {
+    const span = document.createElement('span');
+    const listItem = document.createElement('li');
+    span.innerText = `${coupon}: ${obj[coupon]} %` 
+    listItem.append(span);
+    unsignedList.append(listItem);
+  }
+  cuponsAvailable.append(unsignedList);
+}
+
+displayCouponsAvailable(coupons);
+
+
+// Classes used in project
 class Discount {
   constructor(price, discount = 0) {
     this.price = price;
@@ -39,11 +67,39 @@ class Discount {
   }
 }
 
+class Coupon {
+  constructor(price, coupon, coupons) {
+    this.price = price;
+    this.coupon = coupon;
+    this.coupons = coupons;
+  }
+  getCoupon() {
+    return this.coupon;
+  }
+  isValidPrice() {
+    if (this.price <= 0) {
+      return false;
+    }
+    else {
+      return true;
+    }
+  }
+  isValidCoupon() {
+    let coupons = Object.keys(this.coupons);
+    let isValid = false;
+    for (let i = 0; i < coupons.length; i++) {
+      if (coupons[i] === this.getCoupon()) {
+        isValid = true;
+        break;
+      }
+    };
+    return isValid;
+  }
+}
+
 // Product discount section
 inputPrice.addEventListener('input', calculatePriceWithDiscount);
 inputDiscount.addEventListener('input', calculatePriceWithDiscount);
-
-// btn.addEventListener('click', calculatePriceWithDiscount);
 
 function calculatePriceWithDiscount() {
   const price = Number(inputPrice.value);
@@ -64,4 +120,20 @@ function calculatePriceWithDiscount() {
 
   resultContainer.classList.add('animated');
   pResult.innerText = 'El nuevo precio con descuento es $' + calculateDiscount.newPrice();
+}
+
+// Discount with coupon section
+
+inputPrice2.addEventListener('input', displayPrice);
+btn.addEventListener('click', calculatePriceWithCoupon);
+
+function displayPrice() {
+  const price = Number(inputPrice2.value);
+  pCouponresult.innerText = price;
+}
+
+function calculatePriceWithCoupon() {
+  const price = Number(inputPrice2.value);
+
+
 }
