54dc64e4e0a022744f5d4435f5b1f1c4dbfa6047
Jose Fernando Fuentes Roa <ing.josefuentes@hotmail.com>
2023-02-21 10:20:43 -0500

    Finalizacion de los archivos con las funciones en JavaScript para el analisis salarial

--- module4/analisis.js
+++ module4/analisis.js
@@ -1,4 +1,20 @@
-// Análisis personal para Juanita
+const empresas = {};
+
+for (const persona of salarios) {
+  for (const trabajo of persona.trabajos) {
+    if (!empresas[trabajo.empresa]) {
+      empresas[trabajo.empresa] = {};
+    }
+    
+    if (!empresas[trabajo.empresa][trabajo.year]) {
+      empresas[trabajo.empresa][trabajo.year] = [];
+    }
+
+    empresas[trabajo.empresa][trabajo.year].push(trabajo.salario);
+  }
+}
+
+console.log({empresas});
 
 function findPerson(personInSearch) {
   return salarios.find(person => person.name == personInSearch);
@@ -34,29 +50,68 @@ function personProjection(namePerson) {
   return newSalary;
 }
 
-//Análisis empresarial
-/*
-  {
-    Industrias Mokepon: {
-      2018: [],
-      2019: [],
-    }
+function medianEnterprise(nameEnterprise, searchYear) {
+  if (!empresas[nameEnterprise]) {
+    console.warn('La empresa buscada no existe');
+    return;
   }
-*/
-const empresas = {};
-
-for (const persona of salarios) {
-  for (const trabajo of persona.trabajos) {
-    if (!empresas[trabajo.empresa]) {
-      empresas[trabajo.empresa] = {};
-    }
-    
-    if (!empresas[trabajo.empresa][trabajo.year]) {
-      empresas[trabajo.empresa][trabajo.year] = [];
-    }
-
-    empresas[trabajo.empresa][trabajo.year].push(trabajo.salario)
+  else if (!empresas[nameEnterprise][searchYear]) {
+    console.warn('La empresa no dió salarios ese año');
+    return;
+  }
+  else {
+    return PlatziMath.calculateMedian(empresas[nameEnterprise][searchYear]);
   }
 }
 
-console.log({empresas});
+function enterpriseProjection(nameEnterprise) {
+  if (!empresas[nameEnterprise]) {
+    console.warn('La empresa buscada no existe');
+  }
+  else {
+    const enterpriseYears = Object.keys(empresas[nameEnterprise]);
+    const listMedianYears = enterpriseYears.map((year) => {
+      return medianEnterprise(nameEnterprise, year)
+    })
+
+    let growthPercentages = [];
+
+    for (let i = 1; i < listMedianYears.length; i++) {
+      const actualSalary = listMedianYears[i];
+      const lastSalary = listMedianYears[i - 1];
+      const growthPercentage = (actualSalary - lastSalary) / lastSalary;
+      growthPercentages.push(growthPercentage);
+    }
+
+    const medianGrowthPercentage = PlatziMath.calculateMedian(growthPercentages);
+
+    const lastMedian = listMedianYears[listMedianYears.length - 1];
+    const newMedian = lastMedian * (1 + medianGrowthPercentage);
+
+    return newMedian;
+  }
+}
+
+function medianGeneral() {
+  const medianList = salarios.map(
+    item => medianPerPerson(item.name
+  ));
+  const median = PlatziMath.calculateMedian(medianList);
+
+  return median;
+}
+
+function medianTop10() {
+  const medianList = salarios.map(
+    item => medianPerPerson(item.name
+  )).sort((a,b) => b - a);
+
+  const limit = medianList.length * 0.1;
+  const top10 = [];
+  for (let i = 0; i < limit; i++) {
+    const element = medianList[i];
+    top10.push(element);
+  }
+  // console.log({medianList, top10});
+  return PlatziMath.calculateMedian(top10);
+}
